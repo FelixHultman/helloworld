fc13afce4cb538e6452c6f4c8bd85358115f4206
FelixHultman <karlfelixhultman@gmail.com>
2024-08-20 15:12:26 +0200

    wordcount

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,25 @@
-
+import java.sql.SQLOutput;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int result =max(10, 5);
-        System.out.println("Maxvärde: " + result);
+        String s = "Hejaaaaaaaaaaa";
+        System.out.println(letterCounter(s));
 
-        float averageValue = average(15.4f, 4.3f, 3.2f);
-        System.out.println("Medelvärde: " + averageValue);
-
-        averageValue = average(15.9f, 4.6f, 3.5f);
-        System.out.println("Medelvärde: " + averageValue);
-
-        float[] values = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
-        System.out.println("Nytt medelvärde: " + average(values));
     }
+    public static int letterCounter(String word) {
+        int count = 0;
 
-    static int max(int tal1, int tal2) {
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i)== 'a') {
+                count++;
+            }
 
-        if (tal1 > tal2) {
-            return tal1;
-        } else {
-            return tal2;
         }
+        return count;
     }
 
-    static float average(int number1, int number2) {
-        return (number1 + number2) / 2.0f;
-    }
-
-    static float average(float number1, float number2) {
-        return (number1 + number2) / 2;
-    }
-
-    static float average(float number1, float number2, float number3) {
-        return (number1 + number2 + number3) / 3;
-    }
-
-
-    static float average(float[] numbers) {
-        float sum = 0;
-        for (int i = 0; i <numbers.length ; i++) {
-            sum += numbers[i];
-        }
-        return sum / numbers.length;
-    }
 }
 
 

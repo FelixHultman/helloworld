5db33f314ff279a6b8b7291eb479959c98be6aec
FelixHultman <karlfelixhultman@gmail.com>
2024-08-20 15:52:40 +0200

    removing numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,31 @@
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
 
         String s = "Hejaaaaaaaaaaa";
-        System.out.println(letterCounter(s));
+       /* System.out.println(letterCounter(s));
 
         System.out.println(wordCompareIgnoreCase("aA", "Aa"));
 
-        System.out.println(reverse("hejsan"));
+        System.out.println(reverse("hejsan"));*/
 
         boolean palin = isPalindrome("racecar");
         System.out.println(palin);
+
+        System.out.println(trimZero("00000123456"));
+        System.out.println(trimZero("000100"));
+    }
+
+    private static String trimZero(String number) {
+
+        for (int i = 0; i <number.length() ; i++) {
+            if(number.charAt(i)!='0'){
+                return number.substring(i);
+            }
+
+
+        }
+    return "";
     }
 
     private static boolean isPalindrome(String word) {
